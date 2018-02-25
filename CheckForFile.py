0f5a8d7cc7e5050ef669436a0b3a2b98c59b696d
Nate <nschrein@nmu.edu>
2018-02-25 14:07:59 -0500

    First Commit

---
+++ CheckForFile.py
@@ -0,0 +1,110 @@
+'''
+*   Author: Nate Schreiner: 04/2016
+*
+*   Check to see if file at given path has been created
+*   If file exists, and is from today, send email that everything is ok
+*   if file does not exist or is not from today send a warning email
+*
+'''
+
+
+import smtplib
+import datetime
+import os.path
+from sys import exit
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+now = datetime.datetime.now()
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
+if(now.month < 10):
+    month = "0" + str(now.month)
+else:
+    month = str(now.month)
+
+if(now.day < 10):
+    day = "0" + str(now.day)
+else:
+    day = str(now.day)
+    
+todays_date = str(now.year) + '/' + str(month) + '/' + str(day)
+
+file_name = 'SFTP-Americollect-Results.L1'
+
+file_path = 'C:/Documents and Settings/train2/Desktop/Americollect/'+file_name
+
+fromaddr = 'penmedautoemail@gmail.com'
+toaddr = 'PIM@penmed.com'
+testaddr = 'nateschreiner6@gmail.com'
+
+
+if(os.path.isfile(file_path)):
+    #check to see if it's from the current day
+    date = modification_date(file_path)
+    new_date = date.strftime('%Y/%m/%d')
+    if(new_date == todays_date):
+        #email SFTP file
+        print("Date modified matched today's date....About to send confirmation email")
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = testaddr
+        msg['Subject'] = 'SFTP Americollect File'
+        body = 'Attached is the copy of the SFTP-Americollect-Results.L1 file which WAS modified today'
+        msg.attach(MIMEText(body, 'plain'))
+        print('this is being attached: ' + file_path)
+        attachment = open(file_path, 'rb')
+        print(attachment)
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment: filename= %s' % file_name)
+        msg.attach(part)
+        
+        
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, '9s0mt1ngE')
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit
+    else:
+        #Send warning email
+        print("Date modified did not match todays date....SENDING WARNING EMAIL")
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = testaddr
+        msg['Subject'] = '**WARRNING** File: SFTP-Americollect-Results.L1 DOES NOT HAVE TODAYS DATE ON IT WHICH INDICATES IT DID NOT RUN TODAY'
+        body = file_path + ' This file does not exist which indicates the query failed today'
+        msg.attach(MIMEText(body, 'plain'))
+    
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, '9s0mt1ngE')
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit
+
+else:
+    #Send warning email
+    print("FILE DID NOT EXIST OR WAS NOT FOUND...About to send warning email")
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = testaddr
+    msg['Subject'] = '**WARRNING** File: SFTP-Americollect-Results.L1 DOES NOT EXIST. CHECK QUERY PC'
+    body = file_path + ' This file does not exist which indicates the query failed today'
+    msg.attach(MIMEText(body, 'plain'))
+    
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, '9s0mt1ngE')
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit
+    
+
---
+++ EventScraper/main.py
@@ -0,0 +1,105 @@
+'''
+* Author: Nate Schreiner 12/2017
+*
+*   Scrapes page at given URL for certain tags.
+*   Writes to two files in local directory
+*   Reads in files to an array, sorts both arrays
+*   then compares arrays for differences.  If differences
+*   are found we send email to given ' toaddr '.   
+*
+'''
+
+fromaddr = 'millionaire.trader8@gmail.com'
+toaddr = ['nschrein@nmu.edu', 'nateschreiner6@gmail.com']
+fromPass = "nathan8u"
+
+#pip install requests
+import requests
+from filecmp import cmp
+#pip install bs4
+from bs4 import BeautifulSoup
+import smtplib
+from sys import exit
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+def formatEvent(eventSoupTag):
+    return eventSoupTag.text+' : https://www.islandresortandcasino.com'+eventSoupTag.a['href']+'\n'
+
+def emailError():
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = ", ".join(toaddr)
+    msg['Subject'] = 'Error Raised in Python scraper script'
+    body = 'There has been an error checking the HTML tag on the website!\n\n'
+    msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, fromPass)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit
+
+def emailNew(theEvents):
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    #msg['To'] = toaddr
+    msg['To'] = ", ".join(toaddr)
+    msg['Subject'] = 'New Event(s) Added - Island Resort and Casino'
+    body = 'These event(s) have been added!\n\n'
+    for i in reversed(theEvents):
+        body = body+i
+    body = body +"\nView them all here \n https://www.islandresortandcasino.com/entertainment/island-showroom"
+    msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, fromPass)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit
+
+scapePath = 'https://www.islandresortandcasino.com/entertainment/island-showroom'
+r = requests.get(scapePath)
+
+#parse html and find events
+soup = BeautifulSoup(r.text,'html.parser')
+events = soup.findAll('h2', attrs={'class':'underline'})
+if(len(events) == 0):
+    emailError()
+    exit()
+
+#formated from soupTag to string "title : link"
+formatedEvents = []
+for i in events:
+    #i are the website's Events Titles
+    formatedEvents.append(formatEvent(i))
+
+
+unreadEvents = []
+with open('secondEvent.txt', 'w+') as rf:
+    rf.truncate()
+    for i in reversed(formatedEvents):
+        rf.write(i)
+
+check_file = open('checkFile.txt', 'r')
+for line in check_file:
+    unreadEvents.append(line)
+
+check_file.close()
+formatedEvents.sort()
+unreadEvents.sort()
+debugList = [x for x in formatedEvents if x not in unreadEvents]
+with open('debugFile.txt', 'w+') as debug:
+    for item in debugList:
+        debug.write(item)
+
+if(formatedEvents == unreadEvents):
+    print("no new events")
+else:
+    check_file = open('checkFile.txt', 'w+')
+    check_file.truncate()
+    for i in reversed(formatedEvents):
+        check_file.write(i)
+    emailNew(formatedEvents)
